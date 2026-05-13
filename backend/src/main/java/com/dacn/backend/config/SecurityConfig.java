c92ca947b4596a4b342c06ad86bce861f1af8ba8
minhtoan-nmt <nguyenminhtoan204@gmail.com>
2026-05-13 21:48:49 +0700

    #181 - [BE][FIX] add origin for deployment fe

--- backend/src/main/java/com/dacn/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/dacn/backend/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
 
-        config.setAllowedOrigins(java.util.List.of("http://localhost:3000"));
+        config.setAllowedOrigins(java.util.List.of("http://localhost:3000", "https://ev-charging-service-gamma.vercel.app"));
         config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(java.util.List.of("*"));
         config.setAllowCredentials(true);
