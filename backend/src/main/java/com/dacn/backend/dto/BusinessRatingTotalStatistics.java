21cf0b21c47b5d65e4d2bd2d2c648add008aba5a
minhtoan-nmt <nguyenminhtoan204@gmail.com>
2026-06-05 15:23:05 +0700

    [BE][FIX] get rating statistics error

--- backend/src/main/java/com/dacn/backend/dto/BusinessRatingTotalStatistics.java
+++ backend/src/main/java/com/dacn/backend/dto/BusinessRatingTotalStatistics.java
@@ -5,9 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
+//@AllArgsConstructor
 @NoArgsConstructor
 public class BusinessRatingTotalStatistics {
     private Long totalNumberOfRatings;
     private double averagePoint;
+
+    public BusinessRatingTotalStatistics(Number totalNumberOfRatings, Number averagePoint) {
+        this.totalNumberOfRatings = totalNumberOfRatings != null ? totalNumberOfRatings.longValue() : 0L;
+        this.averagePoint = averagePoint != null ? averagePoint.doubleValue() : 0.0;
+    }
 }
